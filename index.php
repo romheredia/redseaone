cc06e49962d2bcbd6e78eef07534c65176e6ec66
romheredia <romeric.heredia@outlook.com>
2018-01-22 21:42:26 +1300

    converted to php

--- index.php
+++ index.php
@@ -19,14 +19,16 @@
     </header>
 
     <section id="about-us">
-    <h2>About Us</h2>
-    <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at malesuada libero, eget cursus eros. Vivamus
-    in bibendum ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
-    Morbi nec pretium nunc, in molestie felis. Aliquam finibus erat felis, pellentesque lobortis augue fringilla
-    sit amet. Praesent ornare facilisis mattis. Integer fringilla lacus a orci laoreet lobortis vel nec ligula.
-    Curabitur ut elementum mauris.
-    </p>
+        <div class="container">
+            <h2>About Us</h2>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at malesuada libero, eget cursus eros. Vivamus
+                in bibendum ligula. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+                Morbi nec pretium nunc, in molestie felis. Aliquam finibus erat felis, pellentesque lobortis augue fringilla
+                sit amet. Praesent ornare facilisis mattis. Integer fringilla lacus a orci laoreet lobortis vel nec ligula.
+                Curabitur ut elementum mauris.
+                </p>
+        </div>
     </section>
 
     <?php include 'footer.php'; ?>
