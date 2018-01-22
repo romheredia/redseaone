62bb2c629963afab429cd0131ab1829de8a33b5c
romheredia <romeric.heredia@outlook.com>
2018-01-22 16:46:25 +1300

    changed to php

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<?php include_once("liveaboard.html"); ?>
+<!DOCTYPE html>
+<html>
+
+<?php include 'head.php'; ?>
+
+<body>
+    <?php include 'nav.php'; ?>
+
+    <section id="page-header">
+        <div class="container">
+            <h1>Home</h1>
+        </div>
+    </section>
+
+    <header id="home">
+        <div class="container">
+
+        </div>
+    </header>
+
+
+    <?php include 'footer.php'; ?>
+
+</body>
+
+</html>
